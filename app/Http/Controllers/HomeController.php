cc7cd48e4564d555d174ea47eeca85c7a4cf6bc5
sofia-papa <papa.sofia@outlook.it>
2021-11-08 17:10:48 +0000

    controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    
+    public function index()
+    {
+
+        $movie = Movie::all();
+
+        dd('$movie');
+
+        $data = [
+            'movie' => []
+        ];
+
+        return view('home', $data );
+    }
 }
